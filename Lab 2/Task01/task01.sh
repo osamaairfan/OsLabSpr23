84d195dfeb9b511be0bc0dde20e6ce8f8ab50992
Muhammad Osama Irfan <115397536+osamaairfan@users.noreply.github.com>
2023-04-25 20:42:54 +0500

    Delete Lab 2 directory

--- Lab 2/Task01/task01.sh
+++
@@ -1,8 +0,0 @@
-if [[ $1 < $2 && $1 < $3 ]]; 
-then 
-echo "$1 is the minimum"
-elif [[ $2 < $1 && $2 < $3 ]]; then
-echo "$2 is the minimum"
-else 
-echo "$3 is the minimum"
-fi
--- Lab 2/Task04/task04.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-touch fileNames.txt
-tree >> fileNames.txt
-
